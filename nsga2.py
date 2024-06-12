026b7240680c0cabcbcd4d18de9ffb065150f92e
way <133030423+wayc04@users.noreply.github.com>
2024-06-12 23:11:51 +0800

    Update nsga2.py

--- nsga2.py
+++ nsga2.py
@@ -4,31 +4,31 @@
 import random
 import matplotlib.pyplot as plt
 
-# 优化目标1
+# opt func1
 def func1(x):
     return - (x - 1) ** 2
 
-# 优化目标2
+# opt func2
 def func2(x):
     return - (x + 1) ** 2
 
-# 全局种群属性
-epoch = 100 # 迭代次数
-pop_size = 100 # 种群数量
-min_x = -100 #搜索的最小值
-max_x = 100 #搜索的最大值
-mutation_rate = 0.1 # 变异率
+# global population
+epoch = 100 # epoch
+pop_size = 100 # population size
+min_x = -100 #lower bounder
+max_x = 100 # upper bounder
+mutation_rate = 0.1 #mutation rate
 
-# 初始化种群
+# init
 pop = [random.uniform(min_x, max_x) for _ in range(pop_size)]
-func_1_epoch = [0 for _ in range(epoch)] # 优化目标1的历史记录
-func_2_epoch = [0 for _ in range(epoch)] # 优化目标2的历史记录
+func_1_epoch = [0 for _ in range(epoch)] # opt func1 record
+func_2_epoch = [0 for _ in range(epoch)] # opt func2 record
 
-# 交叉
+# crossover
 def crossover(p1, p2):
     return (p1 + p2) / 2
 
-# 变异
+# mutation
 def mutation(p):
     mutation_rate_now = random.uniform(0, 1)
     if mutation_rate_now > mutation_rate:
@@ -37,7 +37,7 @@ def mutation(p):
         return random.uniform(min_x, max_x)
 
 
-# 快速非支配排序
+# fast non dominated sort
 def non_dominated_sort(fit_func_1, fit_func_2):
     pop_size = len(fit_func_1)
     dominated_solutions = [[] for _ in range(pop_size)]
@@ -82,19 +82,19 @@ def non_dominated_sort(fit_func_1, fit_func_2):
     return fronts
 
 
-# 计算拥挤度
+# 计calculate crowding
 def calculate_crowding_distance(fit_func_1, fit_func_2, front):
     length = len(front)
     crowding_distance = [0 for _ in range(len(fit_func_1))]
 
-    # 按照fit_func_1排序
+    # sorted by it_func_1
     sorted_front_1 = sorted(front, key=lambda x: fit_func_1[x])
     crowding_distance[sorted_front_1[0]] = float('inf')
     crowding_distance[sorted_front_1[-1]] = float('inf')
     for k in range(2, length - 1):
         crowding_distance[sorted_front_1[k]] += (fit_func_1[sorted_front_1[k + 1]] - fit_func_1[sorted_front_1[k - 1]]) / (fit_func_1[sorted_front_1[-1]] - fit_func_1[sorted_front_1[0]])
 
-    # 按照fit_func_2排序
+    # sorted by it_func_2
     sorted_front_2 = sorted(front, key=lambda x: fit_func_2[x])
     crowding_distance[sorted_front_2[0]] = float('inf')
     crowding_distance[sorted_front_2[-1]] = float('inf')
@@ -104,47 +104,47 @@ def calculate_crowding_distance(fit_func_1, fit_func_2, front):
     return crowding_distance
 
 
-# 产生新种群
+# generate new population
 def generate_new_population(population):
-    # 对父代的继承
+ 
     new_population = []
 
     while(len(new_population) < pop_size):
-        # 选择父代
+        # parent
         parent_1 = random.choice(population)
         parent_2 = random.choice(population)
 
-        # 交叉
+        # crossover
         child = crossover(parent_1, parent_2)
 
-        # 变异
+        # mutation
         child = mutation(child)
 
         new_population.append(child)
 
     return new_population + population
 
-# 循环迭代
+# loop
 for itr in range(epoch):
     print('epoch{}'.format(itr))
 
-    # 产生新种群
+    # generate new population
     pop = generate_new_population(pop)
 
-    # 适应度计算
+    # calculate fitness
     fit_function_1 = [func1(pop[i]) for i in range(pop_size * 2)]
     fit_function_2 = [func2(pop[i]) for i in range(pop_size * 2)]
 
-    # 快速非支配排序
+    #  non dominate population
     non_dominate_population = non_dominated_sort(fit_function_1, fit_function_2)
 
-    # 拥挤度计算
+    # compute crowding
     crowding_distance = []
     for i in range(0, len(non_dominate_population)):
         crowding_distance.append(calculate_crowding_distance(fit_function_1, fit_function_2, non_dominate_population[i][:]))
 
     offspring = []
-    # 产生子代
+    # generate offspring
     for i in range(0, len(non_dominate_population)):
 
         if len(offspring) + len(non_dominate_population[i]) <= pop_size:
@@ -160,7 +160,7 @@ for itr in range(epoch):
 
     pop = offspring
 
-    # 记录历史
+    # record
     func_1_epoch[itr] = max(fit_function_1)
     func_2_epoch[itr] = max(fit_function_2)
 
